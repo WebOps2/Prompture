9f595eacc9aacd3f38a6aaee18da95c27d184902
Kosi <kosi.amobioleka@gmail.com>
2025-07-21 21:27:46 -0400

    Fixing production bugs

--- promptboard/src/app/api/search/route.ts
+++ promptboard/src/app/api/search/route.ts
@@ -1,6 +1,9 @@
 
-import { openai, supabase } from "@/lib/supabase-client";
+import { supabase } from "@/lib/supabase-client";
 import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 // const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
