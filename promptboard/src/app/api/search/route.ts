2ab1c0d980407e09137e4b500c96bc4daec9e066
Kosi <kosi.amobioleka@gmail.com>
2025-07-21 21:08:46 -0400

    Implementing Semantic Search

--- promptboard/src/app/api/search/route.ts
+++ promptboard/src/app/api/search/route.ts
@@ -4,8 +4,8 @@ import { createClient } from "@supabase/supabase-js";
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
 const supabase = createClient(
   supabaseUrl || "",
